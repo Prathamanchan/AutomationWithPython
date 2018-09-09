917913395e55dbb22a00b0b412c0fb697e279c2c
Prathamanchan <prathamanchan22@gmail.com>
2018-09-09 23:36:44 +0530

    updated

---
+++ ChromeOpen.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome(executable_path='/home/samsung/Desktop/AutomationWithPython/chromedriver')
+browser.get('https://google.com')
+browser.get("http://karmic.co.in")
